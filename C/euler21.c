b7b46f74346830e12ec8296e3d0547dff399149d
Michael Breedlove <michael.breedlove1@gmail.com>
2010-12-17 18:26:13 -0600

    Updated Problem 21 to not require C99 flags in GCC

--- C/euler21.c
+++ C/euler21.c
@@ -4,7 +4,8 @@
 int sum_divisors(int num)
 {
     int sum=1;
-    for(int i=2; i*i <= num; sum += num % i ? 0 : (i*i == num ? i : i + num/i) , ++i);
+    int i;
+    for(i=2; i*i <= num; sum += num % i ? 0 : (i*i == num ? i : i + num/i) , ++i);
     return sum;
 }
 
